9d7bc2a8512c5f116940f73b65d1c0d8946efa3f
Albert Canales Ros <albertcanalesros@gmail.com>
2022-07-26 08:24:58 +0200

    Patched warp and statusallmons

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "SourceCodePro-Black:size=11", "monospace:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#44475a";
